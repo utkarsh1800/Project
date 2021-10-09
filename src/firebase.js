db97faedb93fa208d3b9cdc0f2a2e79b04c60c3e
utkarsh1800 <55633432+utkarsh1800@users.noreply.github.com>
2021-10-09 18:01:42 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBDlz77ffO0HdW70qIO6_hd8bxOyD9fbFg",
+    authDomain: "intership-listing.firebaseapp.com",
+    projectId: "intership-listing",
+    storageBucket: "intership-listing.appspot.com",
+    messagingSenderId: "131026825229",
+    appId: "1:131026825229:web:7d6ee692bd3e5dc6746544",
+    measurementId: "G-LF39W2DBZE"
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+const db = getFirestore(fire);
+
+// Get a list of cities from your database
+
